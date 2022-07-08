c83f6331f00a62e5b72c0c63528da9916251736e
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-08 11:31:48 +0300

    Added registration logic

--- server/routes/Public/register.js
+++ server/routes/Public/register.js
@@ -1,10 +1,49 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+import CredentialValidation from '../../controllers/validation.js'
+import fetch from 'node-fetch';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const loginData = req.body
-    res.json(loginData)
+
+    const { email, password } = req.body
+    const hashedPass = await bcrypt.hash(password, 5);
+
+    if (CredentialValidation(req.body)) {
+        try {
+            //Checking if username with such email already exists
+            let duplicate = false;
+            await fetch('http://localhost:8080/users')
+                .then(res => res.json())
+                .then(users => {
+                    duplicate = users.some(user => user.email === email)
+                    console.log(duplicate)
+                })
+            //Proceeding only if email is not already in use
+            if (!duplicate) {
+                const response = await fetch('http://localhost:8080/users', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: email,
+                        password: hashedPass
+                    }),
+                    headers: { 'Content-Type': 'application/json' }
+                });
+                await response.json();
+                return res.json({
+                    changes: 1,
+                    msg: 'Registration successfull',
+                })
+            } else {
+                res.status(400).send('User already exists')
+            }
+        } catch (err) {
+            res.status(500).send({ error: `Error:` + err })
+        }
+    } else {
+        res.status(400).send('Incorrect information provided, check and try again')
+    }
 })
 
 export default router;
