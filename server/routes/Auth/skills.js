862b1e9f6bb980f1790c33f9593bf104569bb224
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-08 10:59:47 +0300

    skills route added // missing Middleware

--- server/routes/Auth/skills.js
+++ server/routes/Auth/skills.js
@@ -1,5 +1,37 @@
 import express from 'express'
+import fetch from 'node-fetch';
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const response = await fetch(`http://localhost:8080/skills`);
+    const data = await response.json();
+    res.json(!data.length ? { msg: "0 records available" } : data)
+})
+
+router.post('/', async (req, res) => {
+    const { title, description } = req.body
+    const skill = {
+        title: title,
+        description: description
+    }
+
+    try {
+        if (title && description) {
+            const response = await fetch('http://localhost:8080/skills', {
+                method: 'POST',
+                body: JSON.stringify(skill),
+                headers: { 'Content-Type': 'application/json' }
+            });
+            const data = await response.json();
+            return res.json({ msg: 'Skill Added!' })
+        } else {
+            return res.status(400).send({ msg: 'Data should contain title and description keys' })
+        }
+    } catch (err) {
+        return res.status(400).send({ msg: err })
+    }
+
+})
 
 export default router;
