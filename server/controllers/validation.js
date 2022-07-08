abfa8b429e5051c5f9c1afa118f13ca80a601bd0
RokasSkara <rokas.skarakodas@gmail.com>
2022-07-08 11:21:23 +0300

    updated validation to match current setup

--- server/controllers/validation.js
+++ server/controllers/validation.js
@@ -4,36 +4,21 @@ import Ajv from 'ajv'
 
 const ajv = new Ajv()
 
-const RegSchema = {
+const CredSchema = {
     type: "object",
     properties: {
-        email: { type: "string", pattern: '[a-z0-9]+@[a-z]+\.[a-z]{2,3}$' },
-        password: { type: "string", pattern: '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$' }
-    },
-    required: ["email", "password"],
-}
-
-const LogSchema = {
-    type: "object",
-    properties: {
-        email: { type: "string", pattern: '[a-z0-9]+@[a-z]+\.[a-z]{2,3}$' },
+        email: { type: "string", pattern: '[a-z0-9]+@[a-zA-z]+\.[a-z]{2,3}' },
         password: { type: "string", pattern: '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$' },
     },
     required: ["email", "password"],
 }
 
-const validateLog = ajv.compile(LogSchema)
-const validate = ajv.compile(RegSchema)
+const validate = ajv.compile(CredSchema)
 
-let RegValidation = (data) => {
+let CredentialValidation = (data) => {
+    console.log(data)
     const valid = validate(data)
     return valid
 }
 
-let LogValidation = (data) => {
-    const valid = validateLog(data)
-    return valid
-}
-
-export default RegValidation
-export { LogValidation }
+export default CredentialValidation
